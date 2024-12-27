dca5414fee394ef436f1dc0270a76d5315ae8d76
No <no@no.com>
2024-12-27 09:18:05 +0100

    first commit

---
+++ src/main/java/com/example/demo/PingController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class PingController {
+    @GetMapping
+    public String get() {
+        return "Llegaste";
+    }
+
+}
